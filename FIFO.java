af097caae83bdc7bdbf3dad07389a7e47078467f
Sunwoo Yeom <o55306605@gmail.com>
2025-03-13 12:32:05 -0400

    defined Process Data class that defines variables to be used later

--- FIFO.java
+++ FIFO.java
@@ -1,4 +1,15 @@
-public class FIFO {
+// Sunwoo Yeom
 
-    
+// Input : PID, Arrival_Time, Burst_Time
+// variable needed for WT & TAT : Completion_Time, Waiting_Time, Turnarount_Time
+
+// define Process Data
+class ProcessData{
+    int PID, Arrival_Time, Burst_Time, Completion_Time, Waiting_Time, Turnarount_Time;
+
+    public ProcessData(int PID, int Arrival_Time, int Burst_Time) { //defining or allowing input from text
+        this.PID = PID;
+        this.Arrival_Time = Arrival_Time;
+        this.Burst_Time = Burst_Time;
+    }
 }
